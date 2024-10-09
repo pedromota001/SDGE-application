47a66ecd94d2c8c7b087a8961dcc824338121d80
Pedro Mota <pedromotalindoso@gmail.com>
2024-10-09 15:26:27 -0300

    segundo commit

--- src/main/java/br/com/rondonCompany/SDGE/controller/TurmaController.java
+++ src/main/java/br/com/rondonCompany/SDGE/controller/TurmaController.java
@@ -1,8 +1,20 @@
 package br.com.rondonCompany.SDGE.controller;
 
+import br.com.rondonCompany.SDGE.dto.TurmaDTO;
+import br.com.rondonCompany.SDGE.service.TurmaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 
 public class TurmaController {
+
+    @Autowired
+    private TurmaService turmaService;
+    @GetMapping("/turma")
+    public TurmaDTO obterTurmas(){
+        //implementar retorno para /get em /turma
+        return null;
+    }
 }
--- src/main/java/br/com/rondonCompany/SDGE/service/TurmaService.java
+++ src/main/java/br/com/rondonCompany/SDGE/service/TurmaService.java
@@ -1,8 +1,25 @@
 package br.com.rondonCompany.SDGE.service;
 
 
+import br.com.rondonCompany.SDGE.models.Turma;
+import br.com.rondonCompany.SDGE.repository.ITurmaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TurmaService {
+    @Autowired
+    public ITurmaRepository repositoryTurma;
+
+    public List<Turma> obterTurmas(){
+        //implementar metodo
+        return null;
+    }
+
+
+    public void converteDadosDTO(){
+        //implementar
+    }
 }
