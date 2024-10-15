cb6272ce7803f818aa79a5eb70442e382efaea5b
Pedro Mota <pedromotalindoso@gmail.com>
2024-10-15 20:51:35 -0300

    implementacao de controller do aluno

--- src/main/java/br/com/rondonCompany/SDGE/controller/AlunoController.java
+++ src/main/java/br/com/rondonCompany/SDGE/controller/AlunoController.java
@@ -2,9 +2,7 @@ package br.com.rondonCompany.SDGE.controller;
 
 import br.com.rondonCompany.SDGE.dto.AlunoDTO;
 import br.com.rondonCompany.SDGE.service.AlunoService;
-import br.com.rondonCompany.SDGE.service.TurmaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
