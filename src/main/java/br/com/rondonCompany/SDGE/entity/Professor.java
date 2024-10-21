a5a0eedda457b44e2bf9b6bbffc11221390c29d8
Pedro Mota <pedromotalindoso@gmail.com>
2024-10-21 11:07:23 -0300

    adicao de coordenador no banco de dados

--- src/main/java/br/com/rondonCompany/SDGE/entity/Professor.java
+++ src/main/java/br/com/rondonCompany/SDGE/entity/Professor.java
@@ -1,14 +1,11 @@
 package br.com.rondonCompany.SDGE.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Getter
-@Setter
+
 
 @Entity
 @Table(name = "professores")
@@ -21,6 +18,7 @@ public class Professor {
     private String telefone;
     private String email;
     private String senha;
+    private boolean coordenador;
 
     @ManyToMany(mappedBy = "listaProfessores", cascade = CascadeType.ALL)
     private List<Turma> listaTurmas = new ArrayList<>();
@@ -32,4 +30,76 @@ public class Professor {
             inverseJoinColumns = @JoinColumn(name = "id_disciplina")
     )
     private List<Disciplina> listaDisciplinas = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEspecialidade() {
+        return especialidade;
+    }
+
+    public void setEspecialidade(String especialidade) {
+        this.especialidade = especialidade;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public boolean isCoordenador() {
+        return coordenador;
+    }
+
+    public void setCoordenador(boolean coordenador) {
+        this.coordenador = coordenador;
+    }
+
+    public List<Turma> getListaTurmas() {
+        return listaTurmas;
+    }
+
+    public void setListaTurmas(List<Turma> listaTurmas) {
+        this.listaTurmas = listaTurmas;
+    }
+
+    public List<Disciplina> getListaDisciplinas() {
+        return listaDisciplinas;
+    }
+
+    public void setListaDisciplinas(List<Disciplina> listaDisciplinas) {
+        this.listaDisciplinas = listaDisciplinas;
+    }
 }
